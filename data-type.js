cc50561f6a25646ff3ef673df9778de892a9cdf7
onur <onurb215@hotmail.com>
2024-06-27 16:51:04 +0300

    js1

---
+++ data-type.js
@@ -0,0 +1,63 @@
+// DATA TYPE
+
+z = "5"
+x = true
+y = "onur"
+console.log(x);
+
+console.log(typeof x);
+console.log(typeof y);
+console.log(z);
+console.log(typeof z);
+
+yas = 40
+sayi = 15
+
+sonuc = yas + sayi
+console.log(sonuc);
+
+const pi = Math.PI
+console.log(pi);
+
+// ROUND
+
+let yuvarla = Math.round(pi)
+console.log("ne geliyor:",yuvarla);
+
+// trunch
+let tamkisim = Math.trunc(pi)
+console.log("trunc",tamkisim);
+
+// ceil
+let ceil = Math.ceil(pi)
+console.log("ceil",ceil);
+
+// floor
+
+let floor = Math.floor(pi)
+console.log("floor",floor);
+
+
+
+
+// to Fixed
+
+let fixedSonuc = pi.toFixed(3)
+console.log(fixedSonuc);
+
+
+// random methodu
+
+let rastgele = Math.trunc(Math.random()*10)
+console.log(rastgele);
+
+
+// üs alma
+console.log(Math.pow(5, 3));
+
+
+// STRİNG DATA TYPE
+
+let isim = "onur"
+console.log(typeof(isim));
+
--- variables.js
+++ variables.js
@@ -1,59 +1,37 @@
-// DATA TYPE
-
-z = "5"
-x = true
-y = "onur"
-console.log(x);
-
-console.log(typeof x);
-console.log(typeof y);
-console.log(z);
-console.log(typeof z);
-
-yas = 40
-sayi = 15
-
-sonuc = yas + sayi
-console.log(sonuc);
-
-const pi = Math.PI
-console.log(pi);
-
-// ROUND
-
-let yuvarla = Math.round(pi)
-console.log("ne geliyor:",yuvarla);
-
-// trunch
-let tamkisim = Math.trunc(pi)
-console.log("trunc",tamkisim);
-
-// ceil
-let ceil = Math.ceil(pi)
-console.log("ceil",ceil);
-
-// floor
-
-let floor = Math.floor(pi)
-console.log("floor",floor);
+// Javascript de DEĞİŞKENLER
 
 
 
 
-// to Fixed
-
-let fixedSonuc = pi.toFixed(3)
-console.log(fixedSonuc);
+const tckn = 123456789
+// const ile tanımlanan şeyler feğiştirilemez
 
 
-// random methodu
+let toplam =0
+console.log(toplam);
 
-let rastgele = Math.trunc(Math.random()*10)
-console.log(rastgele);
+toplam = 50
+console.log(toplam);
+
+// let ile tanımlanan şeyler değiştirilebilir
 
 
-// üs alma
-console.log(Math.pow(5, 3));
+// GLOBAL ALAN
+
+const abc = 50
 
 
+
+// LOCAL ALAN
+
+function test() {
+  let deneme = "denendi başarılı olundu"
+  console.log(deneme);
+}
+
+test()
+
+
+
+// VAR
 
