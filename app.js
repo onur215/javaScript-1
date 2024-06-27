f06e1e7e1e309e21295f4fe59ca255fae6c52147
onur <onurb215@hotmail.com>
2024-06-27 16:11:59 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log("burası harici js alanı");
+
+// console.error("hata mesajı")
+
+// console.warn("Uyarı")
+
+// alert("Lütfen Dikkat")
+
+document.write(" <h1> JAVASCRİPT İNTRO </h1>")
---
+++ variables.js
@@ -0,0 +1,32 @@
+// DATA TYPE
+
+z = "5"
+x = true
+y = "onur"
+console.log(x);
+
+console.log(typeof x);
+console.log(typeof y);
+console.log(z);
+console.log(typeof z);
+
+yas = 40
+sayi = 15
+
+sonuc = yas + sayi
+console.log(sonuc);
+
+const pi = Math.PI
+console.log(pi);
+
+// ROUND
+
+let yuvarla = Math.round(pi)
+console.log("ne geliyor:",yuvarla);
+
+// trunch
+
+let tamkisim = Math.trunc(pi)
+console.log("trunc",tamkisim);
+
+
