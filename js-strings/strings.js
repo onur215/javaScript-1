870b8160daa5d9faebc179d15cac47ea5d37d933
onur <onurb215@hotmail.com>
2024-07-10 21:18:24 +0300

    concat

--- js-strings/strings.js
+++ js-strings/strings.js
@@ -41,3 +41,63 @@ console.log(typeof(str3));
 
 //? lenght
 //Prototype
+
+
+//! ************STRING METHODS ************
+// charAt()             Returns the character at the specified index.
+// charCodeAt()	        Returns the Unicode of the character at the specified index.
+// concat()	            Joins two or more strings, and returns a new string.
+// endsWith()	        Checks whether a string ends with a specified substring.
+// fromCharCode()	    Converts Unicode values to characters.
+// includes()	        Checks whether a string contains the specified substring.
+// indexOf()	        Returns the index of the first occurrence of the specified value in a string.
+// lastIndexOf()	    Returns the index of the last occurrence of the specified value in a string.
+// localeCompare()	    Compares two strings in the current locale.
+// match()	            Matches a string against a regular expression, and returns an array of all matches.
+// repeat()	            Returns a new string which contains the specified number of copies of the original string.
+// replace()	        Replaces the occurrences of a string or pattern inside a string with another string, and return a new string without modifying the original string.
+// search()	             Searches a string against a regular expression, and returns the index of the first match.
+// slice()	            Extracts a portion of a string and returns it as a new string.
+// split()	            Splits a string into an array of substrings.
+// startsWith()	        Checks whether a string begins with a specified substring.
+// substr()	            Extracts the part of a string between the start index and a number of characters after it.
+// substring()	        Extracts the part of a string between the start and end indexes.
+// toLocaleLowerCase()	Converts a string to lowercase letters, according to host machine's current locale.
+// toLocaleUpperCase()	Converts a string to uppercase letters, according to host machine's current locale.
+// toLowerCase()	    Converts a string to lowercase letters.
+// toString()	        Returns a string representing the specified object.
+// toUpperCase()	    Converts a string to uppercase letters.
+// trim()	            Removes whitespace from both ends of a string.
+// valueOf()	        Returns the primitive value of a String object.
+
+/* -------------------------------------------------------------------------- */
+/*                              length - Property                             */
+/* -------------------------------------------------------------------------- */
+
+const metin = "Clarusway"
+
+console.log(metin.length);
+
+console.log(metin.toUpperCase());
+
+let text1 = "merhaba"
+let text2 = "JavaScript"
+
+console.log(text1+ " " +text2);
+
+let text = "merhaba"
+text += "clarusway"
+
+console.log(text);
+
+/* -------------------------------------------------------------------------- */
+/*                         concat() immutable=değişmez                        */
+/* -------------------------------------------------------------------------- */
+
+const s1 = "mola saati"
+const s2 = " yaklaşıyor"  
+
+console.log(s1.concat(s2));
+
+const newStr = s1.concat(s2)
+console.log(newStr);
