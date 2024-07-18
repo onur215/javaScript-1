35b014a4553686e64fdefa0136e5f6645c2b026f
onur <onurb215@hotmail.com>
2024-07-18 21:31:18 +0300

    obj

--- js-Objects/object.js
+++ js-Objects/object.js
@@ -13,3 +13,42 @@ car.price = 10000
 console.log(car);
 console.log(car.model);
 
+let house = {} // boş obje tanımlama
+house.room = 4
+console.log(house);
+
+let personel = {
+    name: "Fatma",
+    lastName: "Kara",
+    birth: 1990,
+    isMarried: true,
+    experiences: ["tester", "developer", "devops"],
+    adres: {city: "izmir", street: "begonya", no:5},
+    calculateAge: function(){
+        return new Date().getFullYear() - this.birth
+    },
+    selam: ()=>{
+        return "merhaba"
+    },
+}
+
+console.log(personel.name);
+console.log(personel.lastName);
+console.log(personel.birth);
+console.log(personel.experiences[0]);
+console.log(personel.adres.city);
+console.log(personel.calculateAge());
+console.log(personel.selam());
+
+console.log(personel["birth"]);
+console.log(personel["adres"]["city"]);
+
+personel.phone = "555-55-55"
+personel["nick"] = "sopalı"
+console.log(personel);
+
+personel.name = "Ayşe"
+personel["lastName"] = "Yılmaz"
+console.log(personel);
+
+console.clear()
