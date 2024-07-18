cce2cdcb826146349f2e98a093ffa859cc453e9f
onur <onurb215@hotmail.com>
2024-07-18 20:47:02 +0300

    p

---
+++ js-Objects/object.js
@@ -0,0 +1,15 @@
+const ogrİsim = ["ahmet", "mehmet", "saffet"]
+
+const ogrSoyisim = ["yılmaz", "can", "baki"]
+
+console.log(`${ogrİsim[1]}-${ogrSoyisim[1]}`);
+
+const car = new Object()
+
+car.brand = "BMW"
+car.model = 1990
+car.price = 10000
+
+console.log(car);
+console.log(car.model);
+
